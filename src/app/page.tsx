3e836b792fe75b966573f0b22af1e1a585e83bd1
JITSnamibia <136726393+JITSnamibia@users.noreply.github.com>
2026-02-19 12:10:04 +0200

    Remove top spinner from main share page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Uploader from '../components/Uploader'
 import PasswordField from '../components/PasswordField'
 import StartButton from '../components/StartButton'
 import { UploadedFile } from '../types'
-import Spinner from '../components/Spinner'
 import Wordmark from '../components/Wordmark'
 import CancelButton from '../components/CancelButton'
 import { useMemo } from 'react'
@@ -22,7 +21,6 @@ import AddFilesButton from '../components/AddFilesButton'
 function PageWrapper({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <div className="flex flex-col items-center space-y-5 py-10 max-w-2xl mx-auto px-4">
-      <Spinner direction="up" />
       <Wordmark />
       {children}
     </div>
